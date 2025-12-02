727c4d3c2ff5e4c2b4b41b4295fb75dd4b06af87
Ronald Veldema <Ronald.Veldema@sennheiser.com>
2025-12-02 08:33:42 +0100

    feat: Get the bytes from mac in MacAddress class

--- include/iuring/MacAddress.hpp
+++ include/iuring/MacAddress.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <array>
+#include <string>
+
+#include <cstdlib>
+
 namespace iuring
 {
 class MacAddress
@@ -13,6 +18,15 @@ public:
     const std::string& to_string() const
     {
         return m_value;
+    }
+
+    const std::array<uint8_t, 6> to_bytes() const
+    {
+        std::array<uint8_t, 6> bytes{};
+        std::sscanf(m_value.c_str(), "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
+            &bytes[0], &bytes[1], &bytes[2],
+            &bytes[3], &bytes[4], &bytes[5]);
+        return bytes;
     }
 
 private:
